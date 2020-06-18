1aef64f55e155c23fdd179574eecee070ac12cfb
=Bruno Igor <=brigor7@gmail.com>
2020-06-18 11:49:40 -0300

    Estilization of Country

--- src/components/Countries/Country.js
+++ src/components/Countries/Country.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import css from './country.module.css';
 
 export default class Country extends Component {
   render() {
     const { countries } = this.props;
     return countries.map((country) => {
-      return <li key={country.id}>{country.namePT}</li>;
+      return (
+        <li className={css.flexRow} key={country.id}>
+          <img className={css.avatar} src={country.flag} />
+          {country.namePT}
+        </li>
+      );
     });
   }
 }
