291a5da586a7c2b019f3f6d17a3e3901cef6b06e
=Bruno Igor <=brigor7@gmail.com>
2020-06-18 18:16:40 -0300

    include flag

--- src/components/Header/InfoCountry.js
+++ src/components/Header/InfoCountry.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import css from '../Countries/country.module.css';
 
 export default class InfoCountry extends Component {
   render() {
@@ -7,6 +8,10 @@ export default class InfoCountry extends Component {
       return (
         <div>
           <h4>Details of Country</h4>
+          <span>
+            <img className={css.avatar} src={country.flag} alt={country.name} />
+          </span>
+          <br />
           <label>Country´s name:</label>
           <input type="text" disabled value={country.name} />
           <label>Capital:</label>
