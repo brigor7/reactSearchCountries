8e732867f4aba55e8be8befbdefbe257c8d32586
=Bruno Igor <=brigor7@gmail.com>
2020-06-18 10:24:02 -0300

    create state and search allCountries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      allCountries: [],
+    };
+  }
+  async componentDidMount() {
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const json = await res.json();
+
+    const allCountries = json.map((country) => {
+      return {
+        name: country.name,
+        namePT: country.translations.pt,
+        capital: country.capital,
+        population: country.population,
+        id: country.numericCode,
+        region: country.region,
+        subregion: country.subregion,
+        flag: country.flag,
+        languages: country.languages,
+      };
+    });
+
+    console.log(allCountries);
+
+    this.setState({
+      allCountries,
+    });
+  }
+
   render() {
-    return <div>Search Countries</div>;
+    return (
+      <div>
+        <div>Search of Countries</div>
+        <ul>
+          <li>Teste</li>
+          <li>Teste2</li>
+        </ul>
+      </div>
+    );
   }
 }
