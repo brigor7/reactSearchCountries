063ce4ae78685396024f0eb984d44b53846ef931
=Bruno Igor <=brigor7@gmail.com>
2020-06-18 18:44:55 -0300

    show languages of selected country

--- src/components/Header/InfoCountry.js
+++ src/components/Header/InfoCountry.js
@@ -3,6 +3,13 @@ import css from '../Countries/country.module.css';
 import { formatNumber } from '../../helpers/formatHelpers';
 
 export default class InfoCountry extends Component {
+  showLanguages = (countryLanguages) => {
+    const language = countryLanguages.reduce((acc, curr) => {
+      return acc + curr.name + ', ';
+    }, 0);
+    return language;
+  };
+
   render() {
     const { filter } = this.props;
     return filter.map((country) => {
@@ -28,7 +35,11 @@ export default class InfoCountry extends Component {
           <label>Subregion:</label>
           <input type="text" disabled value={country.subregion} />
           <label>Languages:</label>
-          <input type="text" disabled value={country.languages} />
+          <input
+            type="text"
+            disabled
+            value={this.showLanguages(country.languages)}
+          />
         </div>
       );
     });
